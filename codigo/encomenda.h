c8c25ae71f3e3a4a046bee50b8c36b1a3c1b2b8b
jogp10 <jogp10@gmail.com>
2022-04-06 22:56:13 +0100

    yess

--- codigo/encomenda.h
+++ codigo/encomenda.h
@@ -9,12 +9,30 @@ public:
      * Encomenda
      * @param recompensa recompensa da encomenda
      */
-    explicit Encomenda(unsigned int vol, unsigned int peso, unsigned int recompensa);
+    Encomenda(unsigned int vol, unsigned int peso, unsigned int recompensa);
 
     /**
      * Desconstrutor do objeto Encomenda
      */
     virtual ~Encomenda() = default;
+
+    /**
+     * Retorna volume da encomenda
+     * @return volume
+     */
+    unsigned int getVol() const {return vol;};
+
+    /**
+     * Retorna peso da encomenda
+     * @return peso
+     */
+    unsigned int getPeso() const {return peso;};
+
+    /**
+     * Retorna recompensa da encomenda
+     * @return recompensa
+     */
+    unsigned int getRecompensa() const {return recompensa;};
 };
 
 class ExpressoEncomenda: public Encomenda {
@@ -27,6 +45,12 @@ public:
      * @param duracao duracao da encomenda ( viagem de ida e volta )
      */
     ExpressoEncomenda(unsigned int vol, unsigned int peso, unsigned int recompensa, unsigned int duracao);
+
+    /**
+     * Retorna duracao da encomenda
+     * @return duracao
+     */
+    unsigned int getDuracao() const {return duracao;};
 };
 
 class NormalEncomenda: public Encomenda {
