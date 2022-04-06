cf31b7a07c5436a4b40fba6ec03bf00af8c66412
jogp10 <jogp10@gmail.com>
2022-04-06 22:24:27 +0100

    Encomenda class first draft

--- codigo/encomenda.cpp
+++ codigo/encomenda.cpp
@@ -1,5 +1,10 @@
 #include "encomenda.h"
 
-Encomenda::Encomenda() {
+Encomenda::Encomenda(unsigned int vol, unsigned int peso, unsigned int recompensa): vol(vol),peso(peso),recompensa(recompensa)
+{}
 
-}
+ExpressoEncomenda::ExpressoEncomenda(unsigned int vol, unsigned int peso, unsigned int recompensa, unsigned int duracao): Encomenda(vol, peso, recompensa), duracao(duracao)
+{}
+
+NormalEncomenda::NormalEncomenda(unsigned int vol, unsigned int peso, unsigned int recompensa): Encomenda(vol, peso, recompensa)
+{}
--- codigo/encomenda.h
+++ codigo/encomenda.h
@@ -1,10 +1,42 @@
 
 class Encomenda {
 private:
+    unsigned int vol;
+    unsigned int peso;
+    unsigned int recompensa;
 public:
     /**
-     * Constructor
+     * Encomenda
+     * @param recompensa recompensa da encomenda
      */
-    Encomenda();
+    explicit Encomenda(unsigned int vol, unsigned int peso, unsigned int recompensa);
 
+    /**
+     * Desconstrutor do objeto Encomenda
+     */
+    virtual ~Encomenda() = default;
+};
+
+class ExpressoEncomenda: public Encomenda {
+private:
+    unsigned int duracao;
+public:
+    /**
+     * Encomenda do tipo expresso
+     * @param recompensa recompensa da encomenda
+     * @param duracao duracao da encomenda ( viagem de ida e volta )
+     */
+    ExpressoEncomenda(unsigned int vol, unsigned int peso, unsigned int recompensa, unsigned int duracao);
+};
+
+class NormalEncomenda: public Encomenda {
+private:
+public:
+    /**
+     * Encomenda do tipo normal
+     * @param vol volume da encomenda
+     * @param peso peso da encomenda
+     * @param recompensa recompensa da encomenda
+     */
+    NormalEncomenda(unsigned int vol, unsigned int peso, unsigned int recompensa);
 };
